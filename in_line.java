b4904f48f0966ff57942679f51c6bfbcb1f1bccf
ShivamSalunke <105104785+ShivamSalunke@users.noreply.github.com>
2022-05-09 14:35:21 +0530

    Add files via upload

---
+++ in_line.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class in_line {
+
+    public static void main(String args[]) {
+        System.out.println("Please enter your words");
+        Scanner sc = new Scanner(System.in);
+
+
+        String word = sc.next();
+
+        for(char a : word.toCharArray())
+        {
+            System.out.println(a);
+        }
+
+    }
+
